6bd68a6353f4c3b3629a7c09426393f8cdefd27c
Madina Nurlybekova <mnurlybe@c3r5s4.42prague.com>
2024-04-09 20:28:15 +0200

    CPP02 finalized

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -10,5 +10,13 @@ int main( void )
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+
+    // a.setRawBits(42);
+    // std::cout << a.getRawBits() << std::endl;
+    // c.setRawBits(5);
+    // std::cout << c.getRawBits() << std::endl;
+    // b.setRawBits(11);
+    // std::cout << b.getRawBits() << std::endl;
+
     return 0;
 }
--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -20,5 +20,10 @@ int main( void )
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
     
+    // std::cout << "a is " << a.toFloat() << " as Float" << std::endl;
+    // std::cout << "b is " << b.toFloat() << " as Float" << std::endl;
+    // std::cout << "c is " << c.toFloat() << " as Float" << std::endl;
+    // std::cout << "d is " << d.toFloat() << " as Float" << std::endl;
+
     return 0;
 }
